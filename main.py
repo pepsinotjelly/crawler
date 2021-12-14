d85c3a23f95af1bc50c1b0823c0df86c08823053
sunpengyu.sonia <sonia@sunpengyusoniadeMacBook-Pro.local>
2021-12-15 01:18:02 +0800

    再提交一下

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from scrapy.cmdline import execute
+import os
+import sys
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+    if __name__ == '__main__':
+        sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+        execute(['scrapy', 'crawl', 'books_item'])
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
