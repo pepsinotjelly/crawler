df60a9f226ccb1afdbc2735c3094590292c39bb0
pepsinotjelly <1637597600@qq.com>
2021-12-21 01:24:49 +0800

    优化元素重复问题

--- tutorial/book_crawler/spiders/book_spider.py
+++ tutorial/book_crawler/spiders/book_spider.py
@@ -5,13 +5,6 @@
 @Author  ：Sonia.Suen
 @Date    ：2021/12/13 12:55 PM
 @Desc    ：
-    start_requests(self)
-    必须返回一个可迭代的请求（可以返回一个请求列表或编写一个生成器函数），蜘蛛将从中开始爬行。
-    后续请求将从这些初始请求中依次生成。
-
-    parse()是一个方法，将被调用来处理为每个请求下载的响应。
-    response 参数是一个实例，TextResponse它保存页面内容并有更多有用的方法来处理它。
-    该parse()方法通常解析响应，将抓取的数据提取为 dicts，并找到要遵循的新 URL 并Request从中创建新请求 ( )。
 """
 from abc import ABC
 
@@ -24,19 +17,26 @@ from tutorial.book_crawler.items import BookItem
 class TutorialSpider(CrawlSpider, ABC):
     name = 'books_item'  # 标识 Spider 。它在一个项目中必须是唯一的，即不能为不同的 Spider 设置相同的名称。
     allowed_domains = ['book.douban.com']
-    start_urls = ['https://book.douban.com/top250?start=0']
+    start_urls = ['https://book.douban.com/top250']
     rules = [Rule(LinkExtractor(allow=r'^https://book\.douban\.com/top250\?start=\d+$'),
                   callback='parse_item', follow=False)]
 
     def parse_item(self, response):
-        print(response)
-        book_list = response.xpath('//tr[@class="item"]')
-        for book_item in book_list:
+        item_list = response.xpath('//div[@class="indent"]')
+        book_item = item_list.xpath('//tr[@class="item"]')
+        name_list = book_item.xpath('//div[@class="pl2"]/a/@title').extract()
+        info_list = book_item.xpath('//p[@class="pl"]/text()').extract()  # 出版信息
+        star_list = book_item.xpath('//span[@class="rating_nums"]/text()').extract()  # 星级
+        quote_list = book_item.xpath('//span[@class="inq"]/text()').extract()
+        image_list = book_item.xpath('//img/@src').extract()
+
+        for i in range(len(book_item)):
             book = BookItem()
-            book['name'] = book_item.xpath('//div[@class="pl2"]/a/@title').get()
-            book['info'] = book_item.xpath('//p[@class="pl"]/text()').get()  # 出版信息
-            book['star'] = book_item.xpath('//span[@class="rating_nums"]/text()').get()  # 星级
-            book['quote'] = book_item.xpath('//span[@class="inq"]/text()').get()
-            book['image'] = book_item.xpath('//img/@src').get()
+            book['name'] = name_list[i]
+            book['info'] = info_list[i]
+            book['star'] = star_list[i]
+            book['quote'] = quote_list[i]
+            book['image'] = image_list[i]
             yield book
+
 
