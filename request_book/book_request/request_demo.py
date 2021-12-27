641980ab298f9714c0c4a687b33b83af4f56bb6f
sunpengyu.soniav2 <sunpengyu.soniav2@bytedance.com>
2021-12-27 14:11:43 +0800

    增加request proxy

--- request_book/book_request/request_demo.py
+++ request_book/book_request/request_demo.py
@@ -1,41 +1,75 @@
 import urllib.error
+import http.cookiejar
 from urllib.request import Request, urlopen
 
-origin_url = "https://book.douban.com/top250"
-header = {'User-Agent': 'Mozilla/5.0',
-          'Referer': 'https://book.douban.com/',
-          'Cookie': 'bid=fLAdqpnsfRE; __utmc=30149280; __utmc=81379588; '
-                    '_pk_ref.100001.3ac3=%5B%22%22%2C%22%22%2C1640321301%2C%22https%3A%2F%2Faccounts.douban.com%2F%22'
-                    '%5D; _pk_ses.100001.3ac3=*; __utma=30149280.1503170269.1640066250.1640083125.1640321301.3; '
-                    '__utmz=30149280.1640321301.3.2.utmcsr=accounts.douban.com|utmccn=('
-                    'referral)|utmcmd=referral|utmcct=/; __utma=81379588.95857263.1640066250.1640083125.1640321301.3; '
-                    '__utmz=81379588.1640321301.3.2.utmcsr=accounts.douban.com|utmccn=('
-                    'referral)|utmcmd=referral|utmcct=/; push_noty_num=0; push_doumail_num=0; '
-                    'dbcl2="247235182:DmC9SsZ2iE4"; ck=0Tux; __utmb=81379588.4.10.1640321301; '
-                    '_pk_id.100001.3ac3=021988dc59405135.1640066250.3.1640323617.1640083125.; __utmv=30149280.24723; '
-                    '__utmb=30149280.6.10.1640321301 '
-          }
+
+def request_maker(page):
+    origin_url = "https://book.douban.com/top250"
+    header = {'User-Agent': 'Mozilla/5.0',
+              'Referer': 'https://book.douban.com/',
+              }
+    form_data = {
+        'start': page
+    }
+    data = urllib.parse.urlencode(form_data)
+    url = origin_url + '?' + data
+    request = Request(url, headers=header)
+    return request
+
+
+def cookie_maker(request):
+    filename = './cookie.txt'
+    cookie = http.cookiejar.LWPCookieJar(filename)
+    handler = urllib.request.HTTPCookieProcessor(cookie)
+    opener = urllib.request.build_opener(handler)
+    response = opener.open(request)
+    cookie.save(ignore_discard=True, ignore_expires=True)
+
+
+def cookie_reader(url):
+    cookie = http.cookiejar.LWPCookieJar()
+    cookie.load('./cookie.txt', ignore_discard=True, ignore_expires=True)
+    handler = urllib.request.HTTPCookieProcessor(cookie)
+    opener = urllib.request.build_opener(handler)
+    response = opener.open(url)
+    return response
+
+
+def do_request(is_write, page):
+    # 创建代理
+    proxy_handler = urllib.request.ProxyHandler({
+        'http': '218.78.22.146:443',
+        'http': '223.100.166.3',
+        'http': '113.254.178.224',
+        'http': '115.29.170.58',
+        'http': '117.94.222.233'
+    })
+    # 创建opener
+    opener = urllib.request.build_opener(proxy_handler)
+    # 构造请求
+    request = request_maker(page)
+    # 配置全局opener
+    urllib.request.install_opener(opener)
+    try:
+        response = urlopen(request)
+        html = response.read()
+        f = open("./book_response/bookList_" + str(page) + ".html", "wb")
+        if is_write:
+            f.write(html)
+        f.close
+        print(response.geturl())
+        print(response.getcode())
+        print(response.info())
+        response.close()
+    except urllib.error.HTTPError as e:
+        print(e.reason)
+        print(e.code)
+        print(e.headers)
+
+
 page = 0
-form_data = {
-    'start': page
-}
-data = urllib.parse.urlencode(form_data)
-url = origin_url + '?' + data
-request = Request(url, headers=header)
-is_write = True  # 是否重写本地文件
-try:
-    response = urlopen(request)
-    print(403)
-    html = response.read()  # .decode('UTF-8')
-    f = open("./myBook.html", "wb")
-    if is_write:
-        f.write(html)
-    f.close
-    print(html)
-    print(response.geturl())
-    print(response.getcode())
-    print(response.info())
-    response.close()
-except urllib.error.HTTPError as e:
-    if e.code == 403:
-        print("ERROR:Cookie Expired!")
+do_is_write = False  # 是否重写本地文件,True时重写本地.html文件，false时跳过。
+for i in range(10):
+    page = i * 25
+    do_request(do_is_write, page)
+
---
+++ request_book/book_request/url_filter.py
@@ -0,0 +1,3 @@
+import urllib.error
+from urllib.request import Request, urlopen
+
